c488732a2f2955545937abb02e16157ffe784159
unknown <scolkg@gmail.com>
2023-08-04 14:03:48 +0900

    rewrite...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/topics",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/topics`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
